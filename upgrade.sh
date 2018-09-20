9b7cb35a3462bbd36522f6cbcdc247bcfec88584
大胃王 <253498229@qq.com>
2018-09-21 01:24:09 +0800

    test6

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker build . -t registry.cn-qingdao.aliyuncs.com/wangdali/docs-helm:0.0.1
+docker push registry.cn-qingdao.aliyuncs.com/wangdali/docs-helm:0.0.1
 helm upgrade -f ./docs-helm/values.yaml docs-helm ./docs-helm
